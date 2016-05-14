b0be7186dd093c2291c1516bee6adb9181db8a53
smart2538 <worapon.o@ku.th>
2016-05-15 01:25:37 +0700

    edit routeconfig

--- app/app.component.ts
+++ app/app.component.ts
@@ -14,8 +14,7 @@ import {UserService} from './login/user.service';
 @RouteConfig([
   {path: '/login', name: 'Login', component: LoginComponent, },
   {path: '/courses', name: "Courses", component: CoursesComponent, useAsDefault: true },
-  {path: '/course/:id', name: 'CourseDetail', component: CourseDetailComponent, },
-  {path: '/*other', name: 'Other', redirectTo: ['Courses']}
+  {path: '/course/:id', name: 'CourseDetail', component: CourseDetailComponent, }
 ])
 @Component({
     selector: 'my-app',
